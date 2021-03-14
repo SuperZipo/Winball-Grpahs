18653c149a61f2eb9c007a3593b61f1838cfb318
Yair Vered <yairvered1234@gmail.com>
2021-03-14 15:57:14 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BarComponent } from './components/bar/bar.component';
+import { PieComponent } from './components/pie/pie.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatMenuModule } from '@angular/material/menu/';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BarComponent,
+    PieComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    NoopAnimationsModule,
+    MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/services/groups/groups.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, of, BehaviorSubject } from 'rxjs';
+import { Group } from '../../../../../server/client/src/modles/Group';
+import { User } from '../../../../../server/client/src/modles/User';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class GroupsService {
+
+  private groupsUrl = 'http://localhost:8000/api/groups/';
+  private usersUrl = 'http://localhost:8000/api/players/';
+
+  private httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+
+  private group = new BehaviorSubject<Group[]>([]);
+
+  getGroups(): Observable<Group[]> {
+    return this.http.get<Group[]>(this.groupsUrl + "/all", this.httpOptions);
+  }
+
+  getUsers(): Observable<User[]> {
+    return this.http.get<User[]>(this.usersUrl + "/all", this.httpOptions);
+  }
+
+  constructor(private http: HttpClient) {}
+}
